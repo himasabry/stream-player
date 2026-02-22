d4cb1959f36ac9027b4ea48498e6f8ad190061ab
himasabry <36902869+himasabry@users.noreply.github.com>
2026-02-22 03:39:17 +0200

    Update watch.js

--- api/watch.js
+++ api/watch.js
@@ -11,11 +11,19 @@ export default function handler(req, res) {
   const filePath = path.join(process.cwd(), "channels.json");
   const channels = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-  if (!channels[stream]) {
+  let stream_url = null;
+  // البحث في كل قسم
+  for (const section of Object.values(channels)) {
+    if (section[stream]) {
+      stream_url = section[stream];
+      break;
+    }
+  }
+
+  if (!stream_url) {
     return res.status(404).send("Stream not found");
   }
 
   // Redirect مباشر للبث
-  const stream_url = channels[stream];
   res.redirect(stream_url);
 }
