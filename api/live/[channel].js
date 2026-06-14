9b905b203a8485f583ecb938a1d002a66b0629b0
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-14 04:09:17 +0200

    Update [channel].js

--- api/live/[channel].js
+++ api/live/[channel].js
@@ -1,27 +1,8 @@
 export default async function handler(req, res) {
-  const streams = {
-    super_tv_3: "https://livepeercdn.studio/hls/139bsadvu9tvxqg6/index.m3u8"
-  };
+  const url = "https://livepeercdn.studio/hls/139bsadvu9tvxqg6/index.m3u8";
 
-  const url = streams[req.query.channel];
+  const r = await fetch(url);
+  const text = await r.text();
 
-  if (!url) {
-    return res.status(404).send("Channel not found");
-  }
-
-  try {
-    const r = await fetch(url);
-    const m3u8 = await r.text();
-
-    // 👇 اختبار مهم
-    if (!m3u8.includes("#EXTM3U")) {
-      return res.status(500).send("Invalid stream");
-    }
-
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    return res.status(200).send(m3u8);
-
-  } catch (e) {
-    return res.status(500).send("Fetch failed");
-  }
+  return res.status(200).send(text);
 }
