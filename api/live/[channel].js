a1cfd714da6aa350b923acb6ba5def79c8084834
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-18 18:25:14 +0200

    Delete api directory

--- api/live/[channel].js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const url = "https://livepeercdn.studio/hls/139bsadvu9tvxqg6/index.m3u8";
-
-  const r = await fetch(url);
-  const text = await r.text();
-
-  return res.status(200).send(text);
-}
